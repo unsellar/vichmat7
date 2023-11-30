03219a793af4afae5d47e5212e5fb5024f075ee6
unsellar <blinov.ceva2015@yandex.ru>
2023-11-30 19:02:55 +0300

    kEiler added

--- vichmat7/main.cpp
+++ vichmat7/main.cpp
@@ -2,16 +2,17 @@
 #include <cmath>
 using namespace std;
 
-float func(float x, float y, float a, float b, float c){
+float func(float x, float y){
+    float a = -1.5, b = -2.7, c = 0.6;
     return a * y + b * (x-1) * (x-1);
 }
 
 float eiler(float a, float b, float c){
     float right = 2, x = 0, y = c, h = 0.5;
-    cout << "--------eiler-----------"<<endl;
+    cout << "--------Eiler-----------"<<endl;
     int i = 1;
     while(x < right){
-        y = y + h*func(x, y, a, b, c);
+        y = y + h*func(x, y);
         x += h;
         cout << "y" << i << " = " << y << "      for x = " << x << endl;
         i++;
@@ -20,13 +21,24 @@ float eiler(float a, float b, float c){
 }
 
 float kEiler(float a, float b, float c){
-    
+    float right = 2, x = 0, y = c, h = 0.5;
+    cout << "--------Koshi-Eiler-----------"<<endl;
+    int i = 1;
+    while(x < right){
+        y = h/2 * (func(x, y) + func(x+h, y+h*func(x, y)));
+        x += h;
+        cout << "y" << i << " = " << y << "      for x = " << x << endl;
+        i++;
+    }
     return y;
 }
 
 
+
+
 int main(){
-    float a = -1.5, b = -2.7, c = 0.6; // менять
+    float a = -1.5, b = -2.7, c = 0.6; // менять тут и в func(первая функция)
     eiler(a, b, c);
+    kEiler(a, b, c);
     
 }
